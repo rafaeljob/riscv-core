f2cf51ab31768f5b50d3d4537893231a09f66867
seven-lem <lemoncsgo@gmail.com>
2022-03-31 10:18:12 +0200

    pkg v1

--- src/riscv_pkg.vhd
+++ src/riscv_pkg.vhd
@@ -1,23 +1,32 @@
 ----------------------------------------------------------------------------------
--- Dev by: Rafael Basso
+-- Company: Polytech Montpellier
+-- Engineer: Seven Lemonnier
 -- 
--- Create Date: 10.03.2022 
--- Package Name: riscv_pkg 
+-- Create Date: 10.03.2022 11:05:43
+-- Design Name: Package
+-- Module Name: riscv_pkg - Behavioral
+-- Project Name: RISC-V Core
+-- Target Devices: 
+-- Tool Versions: 
 -- Description: 
 -- 
 -- Dependencies: 
 -- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
 ----------------------------------------------------------------------------------
 
 library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.std_logic_1164.all;
 
-package RV32Istd_p is
+package inst_set is 
 
-    type instruction_type is 
-    ( LUI, AUIPC, JAL, JALR, BEA, BNE, BLT, BGE, BLTU, BGEU, LB, LH, LW, LBU, 
-      LHU, SB, SH, SW, ADDI, SLTI, SLTIU, XORI, ORI, ANDI, SLLI, SRLI, SRAI, 
-      ADD, SUB, SSLL, SLT, SLTU, XXOR, SSRL, xSRA, OOR, AAND, FENCE, ECALL, 
-      EBREAK, INVALID_INST );
-      
-end package;
+    type base_inst_set is 
+    
+        ( LUI, AUIPC, JAL, JALR, BEQ, BNE, BLT, BGE, BLTU, BGEU, LB, LH, LW, LBU, LHU, SB, SH, SW, ADDI,
+         SLTI, SLTIU, XORI, ORI, ANDI, SLLI, SRLI, SRAI, ADD, SUB, SSLL, SLT, SLTU, XXOR, SSRL, SSRA, OOR, AAND, -- (S)SLL, (X)XOR, (S)SRL, (S)SRA, (O)OR, (A)AND
+         ECALL, EBREAK);
+         
+end inst_set;
